a87553145e68e34ebb57ee5b8981ed68e5f3619d
Gabriel Caiana <gabrielcaiana@hotmail.com>
2020-02-02 19:05:46 -0300

    config modal com js

--- works/iniciando FrontEnd/app.js
+++ works/iniciando FrontEnd/app.js
@@ -4,9 +4,12 @@ const cards = document.querySelectorAll('.card')
 for (let card of cards) {
     card.addEventListener('click', function() {
         modalOverlay.classList.add('active')
+        const videoId = card.getAttribute("id")
+        modalOverlay.querySelector('iframe').src = `//youtube.com/embed/${videoId}`
     })
 }
 
 document.querySelector('.close-modal').addEventListener('click', function() {
     modalOverlay.classList.remove('active')
+    modalOverlay.querySelector('iframe').src = ""
 })
