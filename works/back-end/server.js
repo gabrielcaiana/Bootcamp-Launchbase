159407a99369e78ee158e8a4294660d934f7dae6
Gabriel Caiana <gabrielcaiana@hotmail.com>
2020-02-09 21:29:28 -0300

    install nunjucks

--- works/back-end/server.js
+++ works/back-end/server.js
@@ -1,11 +1,28 @@
 //importando express
 const express = require('express')
+    //importando nunjucks
+const nunjucks = require('nunjucks')
+
+//configurando servidor
 const server = express()
 
-server.get('/', function(req, res) {
-    return res.send("Hello, How are you?")
+//configuração para utilização de arquivos estaticos
+server.use(express.static('public'))
+
+//configurando engine nunjucks
+server.set('view engine', 'html')
+
+//configurando caminho para engine
+nunjucks.configure('views', {
+    express: server
 })
 
+//configurando rota 
+server.get('/', function(req, res) {
+    return res.render('index')
+})
+
+//iniciando servidor na porta 5000
 server.listen(5000, function() {
     console.log("Server is Running")
 })
