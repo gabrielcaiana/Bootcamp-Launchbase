e56e1ccdc37f4c2e5714c9e4d2275293b175d087
Gabriel Caiana <gabrielcaiana@hotmail.com>
2020-02-12 22:24:02 -0300

    recebendo sobre do back-end no front-end

--- works/back-end/server.js
+++ works/back-end/server.js
@@ -4,7 +4,6 @@ const express = require('express')
 const nunjucks = require('nunjucks')
 
 const videos = require('./data')
-console.log(videos)
 
 //configurando servidor
 const server = express()
@@ -17,12 +16,24 @@ server.set('view engine', 'njk')
 
 //configurando caminho para engine
 nunjucks.configure('views', {
-    express: server
+    express: server,
+    autoescape: false // corrige erro do nunjucks ao passar código HTML direto dentro de objetos
 })
 
 //configurando rota index
 server.get('/', function(req, res) {
-    return res.render('about')
+    const about = {
+        avatar_url: "https://avatars2.githubusercontent.com/u/26663338?s=460&v=4",
+        name: "Gabriel Caiana",
+        role: "Web Developer",
+        description: 'Apaixonado por programação e Design, conheca meu site: <a target="_blank" href="//gabrielcaiana.com">gabrielcaiana.com</a>',
+        links: [
+            { name: "Github", url: "//github.com/gabrielcaiana" },
+            { name: "Twitter", url: "//twitter.com/guedescgabriel" },
+            { name: "Linkedin", url: "///linkedin.com/in/gabrielcaiana" },
+        ]
+    }
+    return res.render('about', { about })
 })
 
 //configurando rota portfolio
