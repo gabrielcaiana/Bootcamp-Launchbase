004cc4bf1ffa20fc8aaf4032ed5a3d1f763191a3
Gabriel Caiana <gabrielcaiana@hotmail.com>
2020-02-16 18:05:22 -0300

    update

--- Desafio - iniciando back-end/courses.js
+++ Desafio - iniciando back-end/courses.js
@@ -2,18 +2,27 @@ module.exports = [{
         id: 'starter',
         logo: './image/starter.svg',
         module: '3 Módulos',
-        price: 'Gratuito'
+        price: 'Gratuito',
+        description: 'Curso básico de Javascript | React | React Native | Node.js',
+        action: 'Visitar site',
+        site: 'https://rocketseat.com.br/starter'
     },
     {
         id: 'launchbase',
         logo: './image/lauchbase.svg',
         module: '8 Módulos',
-        price: 'Comprar'
+        price: 'Comprar',
+        description: 'Fundamentos de front-end e back-end',
+        action: 'Visitar site',
+        site: 'https://rocketseat.com.br/launchbase'
     },
     {
         id: 'gostack',
         logo: './image/gostack.svg',
         module: '10 Módulos',
-        price: 'Comprar'
+        price: 'Comprar',
+        description: 'Tudo sobre React | React Native',
+        action: 'Visitar site',
+        site: 'https://rocketseat.com.br/gostack'
     }
 ]
